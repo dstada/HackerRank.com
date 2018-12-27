0fd840ccbbc2a158010bbbe3eedaea5b829eb9fe
dstada <dickstada@gmail.com>
2018-12-27 14:25:59 +0100

    test

--- Company Logo.py
+++ Company Logo.py
@@ -3,7 +3,27 @@ import os
 import random
 import re
 import sys
+from operator import itemgetter
 
 
 if __name__ == '__main__':
-    s = input()
+    s = list(input())
+    print(s)
+    s_set = set(s)
+    print(s_set)
+    d = [[c, s.count(c)] for c in s_set]
+    print(d)
+    e = sorted(d)
+    print(e)
+    f = e.sort(key=itemgetter(1), reverse=True)
+    print(f)
+
+"""
+Input:
+aabbbccde
+
+Output:
+b 3
+a 2
+c 2
+"""
