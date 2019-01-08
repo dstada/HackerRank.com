2335a51ef4de5bf087516f11b667dda71f324ca0
dstada <dickstada@gmail.com>
2019-01-08 05:49:47 +0100

    test

---
+++ MaxMin-medium.py
@@ -0,0 +1,16 @@
+def maxMin(k, arr):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    k = int(input())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
