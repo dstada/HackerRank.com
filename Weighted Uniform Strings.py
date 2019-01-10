ad2514fa667c1f0a09e18bd277cda4b60729f5ba
dstada <dickstada@gmail.com>
2019-01-10 14:15:33 +0100

    test

--- Weighted Uniform Strings.py
+++ Weighted Uniform Strings.py
@@ -1,17 +1,22 @@
 def weightedUniformStrings(s, queries):
-    scores = []
-    for i in range(len(s)):
-        scores.append(ord(s[i]) - 96)
-        teller = 1
-        while i + teller != len(s) and s[i] == s[i+teller]:
-            print("Nog eentje")
-            teller += 1
-        else:
-            print("Niet nog meer dezelfde.")
+    teller = 1
+    vorige = ''
+    gewichten = []
+    for c in s:     # Voor elke letter uit s:
+        if c != vorige:
             teller = 1
-            break
+            vorige = c
+            gewichten.append(ord(c) - 96)
+        else:
+            teller += 1
+            gewichten.append(teller * (ord(c) - 96))
 
-    print(scores)
+    print(gewichten)
+    for q in queries:
+        if q in s:
+            return "Yes"
+        else:
+            return "No"
 
 
 if __name__ == '__main__':
@@ -28,7 +33,6 @@ if __name__ == '__main__':
 
 """
 https://www.hackerrank.com/challenges/weighted-uniform-string/problem?h_r=next-challenge&h_v=zen
-
 Input:
 abccddde
 6
@@ -38,7 +42,6 @@ abccddde
 5
 9
 10
-
 Output:
 Yes
 Yes
@@ -46,16 +49,4 @@ Yes
 Yes
 No
 No
-
-def weightedUniformStrings(s, queries):
-    scores = set()
-    ctr = 1
-    for i in range(len(s)):
-        score = ord(s[i]) - 96
-        if i + 1 != len(s) and s[i + 1] == s[i]:
-            ctr = ctr + 1
-        else:
-            1
-        scores.add(score * ctr)
-    print(scores)
 """
