ee2b83a7d845fd3e2973e5e85018da53c6facd83
dstada <dickstada@gmail.com>
2019-01-15 14:03:02 +0100

    test

--- Mark and Toys (Interview preparation).py
+++ Mark and Toys (Interview preparation).py
@@ -1,15 +1,19 @@
 
 
 def maximumToys(prices, k):
-    print(prices)
-    print(k)
-    prices2 = [i for i in prices if i <= k]
-    print(prices2)
-    import itertools
-    out_list = []
-    for i in range(1, len(prices2) + 1):
-        out_list.extend(itertools.combinations(prices2, i))
-    print(out_list)
+    prices = sorted(prices)
+    if prices[0] > k:
+        return 0
+    else:
+        sum = 0
+        counter = 0
+        for price in prices:
+            sum += price
+            if sum <= k:
+                counter += 1
+            else:
+                break
+        return counter
 
 
 if __name__ == '__main__':
@@ -18,6 +22,8 @@ if __name__ == '__main__':
     k = int(nk[1])
     prices = list(map(int, input().rstrip().split()))
     result = maximumToys(prices, k)
+    print(result)
+
 
 """
 https://www.hackerrank.com/challenges/mark-and-toys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
@@ -27,4 +33,27 @@ https://www.hackerrank.com/challenges/mark-and-toys/problem?h_l=interview&playli
 
 Output:
 4
+(= An integer that denotes the maximum number of toys Mark can buy for his son.)
+
+Not working for all caes:
+
+def maximumToys(prices, k):
+    if k < min(prices):
+        print(k, sum(prices), min(prices))
+        return 0
+    else:
+        prices2 = [i for i in prices if i <= k]   # Maak array van alle prices die niet groter dan budget zijn
+        import itertools
+        out_list = []
+        # Maak list van alle mogelijke combinaties
+        for i in range(1, len(prices2) + 1):
+            out_list.extend(itertools.combinations(prices2, i))
+        print(out_list)
+        # Maak list van alle combinaties die samen niet groter dan budget zijn:
+        totals = []
+        for comb in out_list:
+            if sum(comb) <= k:
+                totals.append(len(comb))
+        print(totals)
+        print(max(totals))
 """
