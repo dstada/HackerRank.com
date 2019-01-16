714241725d51d019bbcafe5078fc334b0a5c5558
Dick Stada <dickstada@gmail.com>
2019-01-16 22:23:04 +0100

    klaar

--- Collections.deque().py
+++ Collections.deque().py
@@ -1,14 +1,20 @@
 from collections import deque
-
+d = deque()
 for i in range(int(input())):
     cn = input().split()
-    print(len(cn))
+    if cn[1]:
+        cmd_string = "d."+cn[0]+"("+cn[1]+")"
+    else:
+        cmd_string = "d." + cn[0]
+    print(cmd_string)
+    eval(cmd_string)
+    print(d)
+
 
 
 
 """
 https://www.hackerrank.com/challenges/py-collections-deque/problem
-
 Perform append, pop, popleft and appendleft methods on an empty deque d.
 Input:
 6
@@ -18,7 +24,15 @@ append 3
 appendleft 4
 pop
 popleft
-
-Output;
+Output:
 1 2
+
+>>> from collections import deque
+>>> d = deque()
+>>> d.append(1)
+>>> print d
+deque([1])
+>>> d.appendleft(2)
+>>> print d
+>>> d.pop()
 """
