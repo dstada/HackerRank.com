de2672daadfe9a89ba428bd7473ba3c6fde6d007
dstada <dickstada@gmail.com>
2019-01-16 16:54:05 +0100

    test

---
+++ Collections.deque().py
@@ -0,0 +1,24 @@
+from collections import deque
+
+for i in range(int(input())):
+    cn = input().split()
+    print(len(cn))
+
+
+
+"""
+https://www.hackerrank.com/challenges/py-collections-deque/problem
+
+Perform append, pop, popleft and appendleft methods on an empty deque d.
+Input:
+6
+append 1
+append 2
+append 3
+appendleft 4
+pop
+popleft
+
+Output;
+1 2
+"""
