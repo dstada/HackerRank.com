b68dff71af46d8789e937bd19e1439e1f17ecbc1
dstada <dickstada@gmail.com>
2019-01-17 14:01:54 +0100

    test

--- Detect Floating Point Number re.py
+++ Detect Floating Point Number re.py
@@ -1,4 +1,43 @@
 import re
 
 for _ in range(input()):
-	print bool(re.match(r'^[-+]?[0-9]*\.[0-9]+$', raw_input()))
+    print(bool(re.match(r'^[-+]?[0-9]*\.?[0-9]*+$', input())))
+
+
+"""
+https://www.hackerrank.com/challenges/introduction-to-regex/problem
+
+4
+4.0O0
+-1.00
++4.54
+SomeRandomStuff
+
+Output:
+
+False
+True
+True
+False
+
+Your task is to verify that N is a floating point number.
+
+In this task, a valid float number must satisfy all of the following requirements:
+
+ Number can start with +, - or . symbol. 
+For example: 
+✔ +4.50 
+✔ -1.0 
+✔ .5 
+✔ -.7 
+✔ +.4 
+✖  -+4.5
+
+Number must contain at least 1 decimal value. 
+For example: 
+✖ 12. 
+✔ 12.0  
+
+ Number must have exactly one . symbol. 
+ Number must not give any exceptions when converted using floating number.
+"""
---
+++ Minimum Absolute Difference in an Array (Interview Prep).py
@@ -0,0 +1,26 @@
+import itertools
+
+def minimumAbsoluteDifference(arr):
+    combs = list(itertools.combinations(arr, 2))
+    # aantal coms is (len(arr)^2 - len(arr))/2
+    print(combs[0])
+
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().rstrip().split()))
+    result = minimumAbsoluteDifference(arr)
+
+
+"""
+https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=greedy-algorithms
+
+5
+1 -3 71 68 17
+
+Output:
+3
+
+"""
