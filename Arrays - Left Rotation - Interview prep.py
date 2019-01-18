77fa9c2c3785b98040fd20848132946afe089136
dstada <dickstada@gmail.com>
2019-01-18 13:03:59 +0100

    test

---
+++ Arrays - Left Rotation - Interview prep.py
@@ -0,0 +1,27 @@
+import collections
+
+def rotLeft(a, d):
+    a_deque = collections.deque(a)
+    for i in range(d):
+        a_deque.append(a_deque[0])
+        a_deque.popleft()
+    print(a_deque)
+
+
+
+if __name__ == '__main__':
+    nd = input().split()
+    n = int(nd[0])
+    d = int(nd[1])
+    a = list(map(int, input().rstrip().split()))
+    result = rotLeft(a, d)
+    print(result)
+
+"""
+Sample Input
+5 4
+1 2 3 4 5
+
+Sample Output
+5 1 2 3 4
+"""
