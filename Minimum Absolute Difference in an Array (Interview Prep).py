41751e8511783633fde46217a159409c63dffcf8
dstada <dickstada@gmail.com>
2019-01-18 11:35:39 +0100

    test

--- Minimum Absolute Difference in an Array (Interview Prep).py
+++ Minimum Absolute Difference in an Array (Interview Prep).py
@@ -1,12 +1,8 @@
-import itertools
+
 
 def minimumAbsoluteDifference(arr):
-    combs = list(itertools.combinations(arr, 2))
-    # aantal coms is (len(arr)^2 - len(arr))/2
-    mins = []
-    for i in range(len(combs)):
-        mins .append(abs(combs[i][0]-combs[i][1]))
-    return min(mins)
+    arr.sort()
+    return min([arr[teller+1]-arr[teller] for teller in range(len(arr)-1)])
 
 
 if __name__ == '__main__':
@@ -21,8 +17,18 @@ https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/pr
 
 5
 1 -3 71 68 17
+Output: 3
 
-Output:
 3
+3 -7 0
+Output: 3
+
+def minimumAbsoluteDifference(arr):
+    combs = list(set(list(itertools.combinations(arr, 2))))
+    # aantal coms is (len(arr)^2 - len(arr))/2
+    mins = []
+    for i in range(len(combs)):
+        mins .append(abs(combs[i][0]-combs[i][1]))
+    return min(mins)
 
 """
