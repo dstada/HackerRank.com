a7b93f48e510a24188efb90bd2bdec95bdc9359b
Dick Stada <dickstada@gmail.com>
2019-01-19 12:36:23 +0100

    klaar

--- Luck Balance.py
+++ Luck Balance.py
@@ -1,21 +1,18 @@
 def luckBalance(k, contests):
-    print(k)
-    print(contests)
-    imp = []
-    unimp = []
+    contests.sort(reverse=True)
+    luck = 0
+    imp = 0
+
     for cont in contests:
-        if cont[1] == 1:
-            imp.append(cont[0])
+        if cont[1] == 0:    # An important contest
+            luck += cont[0]
+        elif imp < k:       # An  unimportant contest, if nr of imp. contests < k
+            luck += cont[0] # luck grows
+            imp += 1        # imp grows
         else:
-            unimp.append(cont[0])
-    imp.sort()
-    print(imp)
-    unimp.sort()
-    print(unimp)
+            luck -= cont[0] # Unimportant contest and imp less than k
 
-
-
-    return 10
+    return luck
 
 
 
@@ -38,4 +35,22 @@ if __name__ == '__main__':
 5 0
 Output
 29
+
+# Not for all cases right:
+
+def luckBalance(k, contests):
+    # imp = []
+    # unimp = []
+    # for cont in contests:
+    #     if cont[1] == 1:
+    #         imp.append(cont[0])
+    #     else:
+    #         unimp.append(cont[0])
+    # imp.sort()
+    # print(imp)
+    # unimp.sort()
+    # print(unimp)
+    # # print(sum(imp[-k:])+sum(unimp))
+    # # print(sum(imp[:len(imp)-k]))
+    # return sum(imp[-k:])+sum(unimp) - sum(imp[:len(imp)-k])
 """
