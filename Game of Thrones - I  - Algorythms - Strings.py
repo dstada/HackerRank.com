ad0bd2a452dd49062fbf0848aaf9bcbb70fd258d
Dick Stada <dickstada@gmail.com>
2019-01-20 10:55:14 +0100

    klaar

--- Game of Thrones - I  - Algorythms - Strings.py
+++ Game of Thrones - I  - Algorythms - Strings.py
@@ -1,11 +1,23 @@
-
+from collections import Counter
 
 def gameOfThrones(s):
-
-
+    odd = 0
+    for value in Counter(s).values():
+        if value % 2 != 0:
+            odd += 1
+    if odd > 1:
+        return "NO"
+    else:
+        return "YES"
 
 
 if __name__ == '__main__':
     s = input()
     result = gameOfThrones(s)
     print(result)
+
+"""
+cdefghmnopqrstuvw --> No
+
+cdcdcdcdeeeef --> Yes
+"""
