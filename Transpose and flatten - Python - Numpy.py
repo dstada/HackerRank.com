04a6c631a33de2dee8a7e741271ed35e8abf177d
Dick Stada <dickstada@gmail.com>
2019-01-24 22:15:43 +0100

    klaar

---
+++ Transpose and flatten - Python - Numpy.py
@@ -0,0 +1,23 @@
+
+import numpy
+
+n, m = map(int, input().split())
+# (n = rows and n = columns)
+
+my_array = numpy.array([input().split() for _ in range(n)], int)
+print(my_array.transpose())
+print(my_array.flatten())
+
+
+"""
+Input:
+2 2
+1 2
+3 4
+Output
+[[1 3]
+ [2 4]]
+[1 2 3 4]
+
+https://www.hackerrank.com/challenges/np-transpose-and-flatten/problem?h_r=next-challenge&h_v=zen
+"""
