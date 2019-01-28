535484eb68997cd7ec2e91451428a4991b3a014d
dstada <dickstada@gmail.com>
2019-01-28 09:06:58 +0100

    test

--- Maximum Perimeter Triangle - Algorythms.py
+++ Maximum Perimeter Triangle - Algorythms.py
@@ -1,25 +1,16 @@
-import itertools
+n = int(input())
+sticks = sorted(int(i) for i in input().split())
+# sticks is op volgorde van laag naar hoog
 
-def maximumPerimeterTriangle(sticks):
-    combs = list(itertools.combinations(sticks, 3))
-    max_peri = -1
-    max_comb = 0
-    for comb in combs:
-        if comb[0] + comb[1] > comb[2]:
-            if comb[0]+comb[1]+comb[2] > max_peri:
-                max_peri = comb[0]+comb[1]+comb[2]
-                max_comb = combs.index(comb)
-    if max_peri == -1:
-        return -1
-    else:
-        return combs[max_comb]
+i = n-3
+# Beginnen bij de laatste drie en dan naar links toe werken zolang geen driehoek mogelijk is:
+while i >= 0 and (sticks[i] + sticks[i+1] <= sticks[i+2]):
+    i -= 1
 
-if __name__ == '__main__':
-    n = int(input())
-    sticks = list(map(int, input().rstrip().split()))
-    result = maximumPerimeterTriangle(sticks)
-    print(result)
-
+if i >= 0:
+    print(sticks[i], sticks[i+1], sticks[i+2])
+else:
+    print(-1)
 
 """
 https://www.hackerrank.com/challenges/maximum-perimeter-triangle/problem?h_r=next-challenge&h_v=zen
@@ -37,3 +28,27 @@ Input:
 Output
 1000000000 1000000000 1000000000
 """
+# Onderstaande werkt niet voor alle cases:
+# import itertools
+#
+# def maximumPerimeterTriangle(sticks):
+#     combs = list(itertools.combinations(sticks, 3))
+#     max_peri = -1
+#     max_comb = 0
+#     for comb in combs:
+#         if comb[0] + comb[1] > comb[2]:
+#             if comb[0]+comb[1]+comb[2] > max_peri:
+#                 max_peri = comb[0]+comb[1]+comb[2]
+#                 max_comb = combs.index(comb)
+#     if max_peri == -1:
+#         return -1
+#     else:
+#         return combs[max_comb]
+
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     sticks = list(map(int, input().rstrip().split()))
+#     result = maximumPerimeterTriangle(sticks)
+#     print(result)
+#
