c01d564d97e18a1789c616388d5d2072b1b7c72f
dstada <dickstada@gmail.com>
2019-01-30 15:39:46 +0100

    test

--- Common Child - Interview prep.py
+++ Common Child - Interview prep.py
@@ -1,19 +1,40 @@
+import difflib
+
+
 def commonChild(s1, s2):
+    s1a, s2a = "", ""
+    for letter in s1:
+        if letter in s2:
+            s1a += letter
+    for letter in s2:
+        if letter in s1:
+            s2a += letter
+    print(s1a, s2a)
+    matcher = difflib.SequenceMatcher(
+        None, s1a, s2a)
+    match = matcher.find_longest_match(
+        0, len(s1a), 0, len(s2a))
+    return match.size
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     s1 = input()
-
     s2 = input()
-
     result = commonChild(s1, s2)
+    print(result)
+
 
 """
 https://www.hackerrank.com/challenges/common-child/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings&h_r=next-challenge&h_v=zen
 
 Input:
-HARR
+WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS
+FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC
+
+Output: 15
+
+Input:
+HARRY
 SALLY
 
 Output: 2
