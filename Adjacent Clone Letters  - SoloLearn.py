1282faaec9ddd0b9811f20b45bd58f1b348d2ccd
dstada <dickstada@gmail.com>
2019-02-04 15:46:24 +0100

    test

--- Adjacent Clone Letters  - SoloLearn.py
+++ Adjacent Clone Letters  - SoloLearn.py
@@ -21,31 +21,42 @@ for letter in set(new_list):
 print(grid_dict)
 # print(grid[2][3])       # Eerst regel, dan x-e letter van die regel
 for i in range(0, hor):
-    for j in range(0,ver):
+    for j in range(0, ver):
         print(grid[i][j])
-        if i-1 >= 0:        # boven
-            print("boven: {}".format(grid[i-1][j]))
-            if grid[i][j] == grid[i-1][j]:
-                grid_dict[grid[i][j]] += 1
-        if i+1 < len(grid):  # onder
-            print("onder: {}".format(grid[i+1][j]))
         if i-1 >= 0 and j-1 >= 0:   # linksboven
             print("linksboven: {}".format(grid[i-1][j-1]))
-        if i-1 >= 0 and j+1 < len(grid):
+            if grid[i][j] == grid[i-1][j-1]:
+                grid_dict[grid[i][j]] += 1
+        if i - 1 >= 0:  # boven
+            print("boven: {}".format(grid[i - 1][j]))
+            if grid[i][j] == grid[i - 1][j]:
+                grid_dict[grid[i][j]] += 1
+        if i-1 >= 0 and j+1 < ver:        # --------------------------
             print("rechtsboven: {}".format(grid[i-1][j+1]))
-        if i + 1 < len(grid) and j + 1 < len(grid):
-            print("rechtsonder: {}".format(grid[i + 1][j + 1]))
-        if i+1 < len(grid) and j-1 >= 0:
-            print("linksonder: {}".format(grid[i+1][j-1]))
+            if grid[i][j] == grid[i-1][j+1]:
+                grid_dict[grid[i][j]] += 1
         if j-1 >= 0:        # links
             print("links: {}".format(grid[i][j-1]))
-        if j+1 < len(grid):  # rechts
+            if grid[i][j] == grid[i][j-1]:
+                grid_dict[grid[i][j]] += 1
+        if j+1 < ver:  # rechts        # --------------------------
             print("rechts: {}".format(grid[i][j+1]))
-
-        print("--------------")
+            if grid[i][j] == grid[i][j+1]:
+                grid_dict[grid[i][j]] += 1
+        if i+1 < hor and j-1 >= 0:        # --------------------------
+            print("linksonder: {}".format(grid[i+1][j-1]))
+            if grid[i][j] == grid[i+1][j-1]:
+                grid_dict[grid[i][j]] += 1
+        if i+1 < hor:  # onder        # --------------------------
+            print("onder: {}".format(grid[i+1][j]))
+            if grid[i][j] == grid[i+1][j]:
+                grid_dict[grid[i][j]] += 1
+        if i + 1 < hor and j + 1 < ver:        # --------------------------
+            print("rechtsonder: {}".format(grid[i + 1][j + 1]))
+            if grid[i][j] == grid[i+1][j+1]:
+                grid_dict[grid[i][j]] += 1
 
 print(grid_dict)
-
 
 
 """
