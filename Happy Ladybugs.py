e98c6f070558340437a623392aed2e23bcce90e1
dstada <dickstada@gmail.com>
2019-02-05 14:20:22 +0100

    test

--- Happy Ladybugs.py
+++ Happy Ladybugs.py
@@ -1,16 +1,30 @@
+def check_happy(b):
+    overall_happy = True
+    for i in range(len(b)):
+        print(i)
+        happy = False
+        # If left or right is the same colour bug, then happy:
+        if (i - 1 >= 0 and b[i - 1] == b[i]) or i + 1 < len(b) and b[i + 1] == b[i]:
+            happy = True
+        else:
+            print("Niet happy")
+            overall_happy = False
+        print(happy)
+    return overall_happy
+
+
 def happyLadybugs(b):
-    print(b)
     # At least one char is the only of that kind:
     for letter in b:
         if letter != '_' and b.count(letter) < 2:
-            print("NO")
-            break
+            return "NO"
+            # break
     # Check if existing ladybugs are happy now:
-    for i in range(len(b)):
-        print(i)
-
-
-    return 10
+    if check_happy(b) is True:
+        return "YES"
+    else:
+        #TODO: Kijk hoe de ladybugs toch blij kunnen worden
+        print("Kijken of ze blij kunnen worden")
 
 
 if __name__ == '__main__':
