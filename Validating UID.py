f7bcc951fc89da34597bd5503a255278424da822
Dick Stada <dickstada@gmail.com>
2019-02-06 22:07:55 +0100

    klaar

---
+++ Validating UID.py
@@ -0,0 +1,27 @@
+
+
+
+"""
+https://www.hackerrank.com/challenges/validating-uid/problem
+
+It must contain at least 2 uppercase English alphabet characters.
+It must contain at least 3 digits (0 - 9).
+It should only contain alphanumeric characters (a -z , A -Z & 0 - 9).
+No character should repeat.
+There must be exactly 10 characters in a valid UID.
+
+Input:
+
+2
+B1CD102354
+B1CDEF2354
+
+Output:
+Invalid
+Valid
+
+Explanation
+
+B1CD102354: 1 is repeating → Invalid
+B1CDEF2354: Valid
+"""
