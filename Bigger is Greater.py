fb7294c0621e7519f1358dff721879df4ea77e7f
Dick Stada <dickstada@gmail.com>
2019-02-18 22:30:47 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

--- Bigger is Greater.py
+++ Bigger is Greater.py
@@ -14,13 +14,28 @@ def biggerIsGreater(w):
     # van achteraf naar voren kijken waar eerste waarde zit die kleiner is dan rechter buren:
     for i in range(len(w) - 1):
         if lst[len(lst)-i-1] > lst[len(lst) - i - 2]:
-            print("Rechter buurman van {} groter!".format(lst[len(lst) - i - 2]))
+            center = lst[len(lst) - i - 2]
+
             print("rechter buren: {}".format(lst[len(lst) - i - 1:]))
             rest_lst = sorted(lst[len(lst) - i - 1:])
             print("rest_lst: {}".format(rest_lst))
+
             part1 = lst[:len(lst) - i - 2]
-            print("deel 1: {}".format(lst[:len(lst) - i - 2]))
+            print("part 1: {}".format(lst[:len(lst) - i - 2]))
+
             # Dus: 1) vervang de eerste vanaf rechts waarvan de waarde kleiner is dan rechter buurman,
+            right = lst[len(lst) - i - 1:]
+            right_ord = sorted(right)
+            print(right_ord)
+            # Zoek de eerstvolgend qua grootte:
+            for value in right_ord:
+                if value > center:
+                    part2 = value   #
+                    print(part2)
+                    right_ord.remove(value)    # haal de vervanger van center uit right_ord
+                    print(right_ord)
+                    right_ord.append(center)    # voeg de te vervangen waarde aan rechter deel toe
+                    print(sorted(right_ord))
 
 
             # door de kleinste van de rechter buurmannen en zet de rest in oplopende volgorde.
