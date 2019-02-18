48f428e7a3599f9d6a1da24a303e4e443802588f
dstada <dickstada@gmail.com>
2019-02-18 16:45:33 +0100

    test

--- Bigger is Greater.py
+++ Bigger is Greater.py
@@ -3,10 +3,24 @@ def biggerIsGreater(w):
     lst = []
 
     for letter in w:
-        print(ord(letter))
         lst.append(ord(letter))
     print(lst)
 
+    # Kijk of het groter kan
+    # In omgekeerde volgorde sorteren;
+    lst_i = sorted(lst, reverse=True)
+    if lst == lst_i:
+        return "no answer"
+
+    # van achteraf naar voren kijken waar eerste waarde zit die kleiner is dan rechter buren:
+    for i in range(len(w) - 1):
+        if lst[len(lst)-i-1] > lst[len(lst) - i - 2]:
+            print("Rechter buurman van {} groter!".format(lst[len(lst) - i - 2]))
+            print("rechter buren: {}".format(lst[len(lst) - i - 1:]))
+            print("Deze waarde(s) blijft/blijven staan: {}".format(lst[:len(lst) - i - 1]))
+
+            # Dus: 1) vervang de eerste vanaf rechts waarvan de waarde kleiner is dan rechter buurman,
+            # door de kleinste van de rechter buurmannen en zet de rest in oplopende volgorde.
 
 if __name__ == '__main__':
     T = int(input())
@@ -14,6 +28,8 @@ if __name__ == '__main__':
         w = input()
         result = biggerIsGreater(w)
         print(result)
+
+
 
 """
 Input:
