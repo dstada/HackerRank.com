39010c429d706b04eda928c0c8abda476a3a2818
dstada <dickstada@gmail.com>
2019-02-25 11:22:42 +0100

    test

--- Climbing the Leaderboard - Algorythms.py
+++ Climbing the Leaderboard - Algorythms.py
@@ -6,27 +6,20 @@ import sys
 
 # Complete the climbingLeaderboard function below.
 def climbingLeaderboard(scores, alice):
+    print(scores)
+    teller = 1
+    for i in range(len(scores)):
 
 
 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     scores_count = int(input())
-
     scores = list(map(int, input().rstrip().split()))
-
     alice_count = int(input())
-
     alice = list(map(int, input().rstrip().split()))
-
     result = climbingLeaderboard(scores, alice)
 
-    fptr.write('\n'.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
 
 """
 Input:
