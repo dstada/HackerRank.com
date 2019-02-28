4097c0434e352f017227c88d0c5378848d8b78a9
dstada <dickstada@gmail.com>
2019-02-28 10:15:21 +0100

    test

--- Cavity Map - Algorythms.py
+++ Cavity Map - Algorythms.py
@@ -1,13 +1,12 @@
 
 
 def cavityMap(grid):
-    print(grid)
-    arr = [grid[0]]
+    arr = [grid[0]]     # Eerste regel naar nieuwe grid, want geen X mogelijk.
     print(arr)
-    for i in range(1, len(grid) - 1):
-        print(grid[i])
-        new_line = grid[i]
-        print("new_line: {}".format(new_line))
+    for i in range(1, len(grid) - 1):       # Tussenregels checken op cavity
+        print("We bekijken: {}".format(grid[i]))
+        new_line = grid[i]                # eerst de bestaande regel overnemen
+        # print("new_line: {}".format(new_line))
         for j in range(1, len(grid[i]) - 1):
             u = int(grid[i - 1][j])
             l = int(grid[i][j - 1])
@@ -15,9 +14,11 @@ def cavityMap(grid):
             d = int(grid[i + 1][j])
             if u < int(grid[i][j]) and l < int(grid[i][j]) and r < int(grid[i][j]) and d < int(grid[i][j]):
                 print("X")
-                new_line = new_line + 'X'
+                new_line = new_line[:j] + 'X' + new_line[j+1:]
+        print(new_line)
+        arr.append(new_line)
     arr.append(grid[-1])
-    print(arr)
+    return arr
 
 
 if __name__ == '__main__':
@@ -43,4 +44,29 @@ Output:
 1X12
 18X2
 1234
+
+Input:
+989
+191
+111
+
+Output:
+989
+1X1
+111
+
+This one is better:
+
+def cavityMap(grid):  
+    s = ''.join(line for line in grid); t = s[:]
+    l = len(grid[0]); n = len(grid)
+
+    for i in range(l+1, (n-1)*l,l):
+        for j in range(i, i+l-2):
+            values = map(int, [s[j+1], s[j-1], s[j-l], s[j+l]])
+            if int(s[j]) > max(values):
+                t = ''.join([t[:j],'X',t[j+1:]])
+
+    for i in range(0, n*l,l):
+        yield t[i:i+l]
 """
