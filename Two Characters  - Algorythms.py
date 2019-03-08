8acb3a7a13fb703e52f9b74b8aa485ddc9206c35
Dick Stada <dickstada@gmail.com>
2019-03-09 00:06:30 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

---
+++ Two Characters  - Algorythms.py
@@ -0,0 +1,19 @@
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the alternate function below.
+def alternate(s):
+    print(s)
+    return "OK"
+
+if __name__ == '__main__':
+    l = int(input().strip())
+    s = input()
+    result = alternate(s)
+    print(result)
