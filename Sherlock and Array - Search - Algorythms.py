09182e035dcb90fbdda48c52800503ac602d4060
dstada <dickstada@gmail.com>
2019-03-13 17:02:59 +0100

    allmost ready

--- Sherlock and Array - Search - Algorythms.py
+++ Sherlock and Array - Search - Algorythms.py
@@ -1,11 +1,42 @@
 
 
 def balancedSums(arr):
+    if sum(arr[1:]) == 0 or sum(arr[:-1]) == 0:
+        return "YES"
     i = 0
     j = len(arr) - 1
-    while i + j
+    links = arr[i]
+    rechts = arr[j]
+    tot = 2
+    while tot != len(arr) - 1:
+        if links < rechts:
+            i += 1
+            links += arr[i]
+            print("links: {} - rechts: {}".format(links, rechts))
+        elif links > rechts:
+            j -= 1
+            rechts += arr[j]
+            print("links: {} - rechts: {}".format(links, rechts))
+        else:       # links gelijk aan rechts
+            print("Volgenden: links: {} en rechts: {}".format(arr[i+1], arr[j-1]))
+            if arr[i+1] <= arr[j-1]:
+                print("links minder dan rechts")
+                i += 1
+                links += arr[i]
+                print("Links wordt nu: {} en rechts: {}".format(links, rechts))
+            else:
+                print("rechts minder dan links")
+                j -= 1
+                rechts += arr[j]
+                print("links: {} - rechts: {}".format(links, rechts))
+        tot += 1
+    print("links: {} - rechts: {}".format(links, rechts))
+    if links == rechts:
 
-
+        print("gelijk! Dus YES")
+    else:
+        print("niet gelijk... dus NO")
+    print(tot)
 
 
 if __name__ == '__main__':
@@ -17,6 +48,46 @@ if __name__ == '__main__':
         print(result)
 
 """
+input:
+10
+1
+1
+------
+1
+2
+------
+1
+3
+------
+2
+1 2 --> NO
+------
+3
+1 4 1
+3
+1 5 1
+1
+234
+1
+20000
+3
+6 23 6
+1
+1
+
+output:
+YES
+YES
+YES
+NO
+YES
+YES
+YES
+YES
+YES
+YES
+
+-------------
 2
 3
 1 2 3
@@ -37,4 +108,22 @@ YES
 YES
 YES
 YES
+
+Case 6: (wrong but output must be YES
+1
+1
+1
+
+YES
+
+https://www.hackerrank.com/challenges/sherlock-and-array/problem
+
+Time out with case 3 and 4:
+
+def balancedSums(arr):
+    for i in range(len(arr)):
+        # print(sum(arr[:i]), sum(arr[i+1:]))
+        if sum(arr[:i]) == sum(arr[i+1:]):
+            return "YES"
+    return "NO"
 """
