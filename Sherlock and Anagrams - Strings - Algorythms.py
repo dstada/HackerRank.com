4f7b13db5c3d8c2026b797c621459ba164507ef5
dstada <dickstada@gmail.com>
2019-03-14 09:00:18 +0100

    allmost ready

---
+++ Sherlock and Anagrams - Strings - Algorythms.py
@@ -0,0 +1,31 @@
+
+
+def sherlockAndAnagrams(s):
+
+
+
+
+if __name__ == '__main__':
+    q = int(input())
+    for q_itr in range(q):
+        s = input()
+        result = sherlockAndAnagrams(s)
+
+"""
+Input:
+2
+ifailuhkqq
+kkkk
+
+Output 1
+3
+10
+
+Input 2:
+1
+cdcd
+
+Output 2
+5
+
+"""
