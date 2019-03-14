02c93ca3ba33d884e206d6cf8f503ac6eeb70433
dstada <dickstada@gmail.com>
2019-03-14 14:04:24 +0100

    under construction

--- Sherlock and Anagrams - Strings - Algorythms.py
+++ Sherlock and Anagrams - Strings - Algorythms.py
@@ -1,8 +1,16 @@
-
+import re
 
 def sherlockAndAnagrams(s):
-
-
+    for i in range(len(s)+1):       # Make all possible combinations
+        for j in range(i+1, len(s)+1):
+            print(s[i:j])
+            for letter in s[i:j]:
+                list = []
+                print("Letter: {}".format(letter))
+                list = [m.start() for m in re.finditer(letter, s)]
+                print(list)
+                print("index: {}:{}".format(i, j))
+    return ""
 
 
 if __name__ == '__main__':
@@ -10,6 +18,7 @@ if __name__ == '__main__':
     for q_itr in range(q):
         s = input()
         result = sherlockAndAnagrams(s)
+        print(result)
 
 """
 Input:
