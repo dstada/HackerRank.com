fa5ff6b7f155ace5c04930ed1f6620f3c21e4c90
Dick Stada <dickstada@gmail.com>
2019-03-22 20:00:16 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

--- Validating Credit Card Numbers - Python -Regex.py
+++ Validating Credit Card Numbers - Python -Regex.py
@@ -1,16 +1,29 @@
 import re
 
 
+
 for _ in range(int(input())):
-    if bool(re.match(r'^[4-6]{1}[\d]{3}[-|\s]?[\d]{4}[-|\s]?[\d]{4}[-|\s]?[\d]{4}$', input())) is True:
+    if bool(re.match(r'^(?!.*(\d)(-?\1){3})(?!.*(\d)\1{4})[4-6]{1}[\d]{3}[-|\s]?[\d]{4}[-|\s]?[\d]{4}[-|\s]?[\d]{4}$', input())) is True:
         print("Valid")
     else:
         print("Invalid")
 
 
-
 """
+ook kan:
 
+import re
+TESTER = re.compile(
+    r"^"
+    r"(?!.*(\d)(-?\1){3})"
+    r"[456]"
+    r"\d{3}"
+    r"(?:-?\d{4}){3}"
+    r"$")
+for _ in range(int(input().strip())):
+    print("Valid" if TESTER.search(input().strip()) else "Invalid")
+    
+    
 ► It must start with a 4, 5 or 6. 
 ► It must contain exactly 16 digits. 
 ► It must only consist of 0-9 digits (). 
