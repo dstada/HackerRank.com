23aefb3a6a0c03998cbfaaf29c3e7ef79a489a96
dstada <dickstada@gmail.com>
2019-03-25 14:59:36 +0100

    not ready

--- Priyanka and Toys - Greedy - Algorutms.py
+++ Priyanka and Toys - Greedy - Algorutms.py
@@ -1,19 +1,17 @@
 
 
 def toys(w):
-    w = list(set(w))
+    w.sort()
     i = 0
-    containers = 0
-    while i < len(w):
-        unit = []
-        unit.append(w[i])
-        if max(unit) - min(unit) <= 4:
-            i += 1
-        else:
-            units += 1
-            break
-    print(units)
-
+    basis = 0
+    containers = 1
+    while i < len(w):               # Zolang i kleiner dan lengte array
+            if w[i] <= w[basis] + 4:        # Als huidige w[i] kleiner of gelijk aan 'uitgangs-w[]':
+                    i += 1                  # i ophogen
+            else:                       # verschil groter dan 4
+                    basis = i                 # uitgangs-w' verschuiven naar i
+                    containers += 1         # aantal containers ophogen
+    return containers
 
 
 if __name__ == '__main__':
@@ -34,9 +32,25 @@ Output:
 4
 
 Input:
+8
 1 2 3 4 5 10 11 12
 
 Output:
 2
 
+
+def toys(w):
+    w = sorted(list(set(w)))
+    print((w))
+    i = 0
+    min = i
+    containers = 0
+    while i < len(w):                 # Zolang i niet verder dan laatste element van w
+        while w[i] - w[min] <= 4:     # Zolang laatste min eerste niet groter dan 4:
+            i += 1                    # Teller ophogen om volgend element te bekijken
+        else:
+            containers += 1           # grootste min kleinste > 4, dus weer een container
+            min = i                   # min wordt verzet naar nieuwe i
+            i += 1
+    print(containers)
 """
