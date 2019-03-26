36c950a48488a107512116dd52a71fcf8a61c374
Dick Stada <dickstada@gmail.com>
2019-03-26 22:06:15 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

---
+++ Sherlock and The Beast - Greedy - Algorythms.py
@@ -0,0 +1,30 @@
+
+def decentNumber(n):
+
+
+
+if __name__ == '__main__':
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        decentNumber(n)
+
+
+
+"""https://www.hackerrank.com/challenges/sherlock-and-the-beast/problem
+
+input:
+4
+1
+3
+5
+11
+
+output
+-1
+555
+33333
+55555533333
+"""
