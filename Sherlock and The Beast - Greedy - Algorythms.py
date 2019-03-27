9e4f62ed6c48303f40539dc3400c3fea7d3ea2aa
dstada <dickstada@gmail.com>
2019-03-27 16:54:46 +0100

    not ready

--- Sherlock and The Beast - Greedy - Algorythms.py
+++ Sherlock and The Beast - Greedy - Algorythms.py
@@ -1,6 +1,21 @@
 
 def decentNumber(n):
-
+    if n < 3:               # n moet 3 of meer zijn
+        return -1
+    elif n % 3 == 0:        # n deelbaar door 3
+        return int((n / 3)) * "555"
+    elif n % 5 == 0 and n >= 5:        # n deelbaar door 5
+        return int((n / 5)) * "33333"
+    elif n >= 8:            # niet deelbaar door 3 of 5
+        # Eerst alle posities vullen met 5'en
+        # bijv. 11 --> 55555555555
+        for i in range(1, n//5 + 1):
+            print(i)
+            # Vul op vanaf rechts met steeds 5 x een 3, en check of rest deelbaar is door 3:
+            if (n - i * 5) % 3 == 0:    # Als van n x keer 5 3'en afgaan, is dan de rest deelbaar door 5?
+                outpt = ((n - i * 5) // 3) * "555" + i * "33333"
+                print(outpt)
+                break
 
 
 if __name__ == '__main__':
@@ -9,8 +24,7 @@ if __name__ == '__main__':
     for t_itr in range(t):
         n = int(input().strip())
 
-        decentNumber(n)
-
+        print(decentNumber(n))
 
 
 """https://www.hackerrank.com/challenges/sherlock-and-the-beast/problem
