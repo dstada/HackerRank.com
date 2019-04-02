0b133f1d60353bbf4b8d746d04ca8c3e110b7273
Dick Stada <dickstada@gmail.com>
2019-04-02 22:44:21 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

--- Filling Jars.py
+++ Filling Jars.py
@@ -1,14 +1,25 @@
+
+
 def solve(n, operations):
     # n = nr of jars
     # m = nr of rounds
     # operations s bijv. [[1, 2, 100], [2, 5, 100], [3, 4, 100]]
-    jars = [[] for i in range(n)]
-    for j in range(len(operations)):
-        print(j)
-        for x in range(operations[j][0], operations[j][1]+1):
-
-        jars[operations[j] = operations[j][2]
+    # jars = [[] for i in range(n)]
+    jars = [0 for i in range(n)]
+    van = 0
+    tot = 0
+    for item in operations:        # voor elk element van operations
+        van = item[0]
+        tot = item[1]
+        print("van {} tot {}".format(van, tot))
+        for i in range(van - 1, tot):
+            print(i)
+            print(item[2])
+            print(type(item[2]))
+            print(jars[i])
+            jars[i] += item[2]
     print(jars)
+    print(sum(jars) / len(jars))
 
 
 if __name__ == '__main__':
