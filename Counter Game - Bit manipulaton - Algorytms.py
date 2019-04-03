2a78a04f95688f4812331e77d9e81ba179242377
dstada <dickstada@gmail.com>
2019-04-03 16:45:51 +0200

    not ready

--- Counter Game - Bit manipulaton - Algorytms.py
+++ Counter Game - Bit manipulaton - Algorytms.py
@@ -3,21 +3,24 @@ import math
 
 
 def counterGame(n):     # Louise starts first
+    arr = [1, 2]
+    while arr[-1] < n:
+        arr.append(arr[-1]*2)
+    print(arr)
     teller = 0
     while n > 1:        # Zolang 1 nog niet is bereikt het volgende
         teller += 1
-        if ((n & (n-1)) == 0) and n > 0:    # als factor2
-            print("{} is factor2. Nu delen door 2.".format(n))
+        if n in arr:    # als factor2
             n /= 2
         else:                   # Geen factor van 2
-            print("{} geen factor2.".format(n))
-            for i in range(n - 1, 2, -1):       # Zoek eerstvolgende factor 2 getal, terugtellend.
-                if ((i & (i - 1)) == 0) and i > 0:  # Factor 2 getal gevonden
-                    n -= i                          # Van n nu de kleinere factor2 aftrekken
-    # Als het goed is, is n nu 1
-    if n == 1:
-        print("teller: {}".format(teller))
-    print(n)
+    #         for i in range(n - 1, 2, -1):       # Zoek eerstvolgende factor 2 getal, terugtellend.
+    #             if (i & (i - 1)) == 0:  # Factor 2 getal gevonden
+    #                 n -= i                          # Van n nu de kleinere factor2 aftrekken
+    # if teller % 2 == 0:
+    #     return "Richard"
+    # else:
+    #     return "Louise"
+
 
 
 
@@ -32,7 +35,20 @@ if __name__ == '__main__':
 """https://www.hackerrank.com/challenges/counter-game/problem
 
 
-
+def counterGame(n):     # Louise starts first
+    teller = 0
+    while n > 1:        # Zolang 1 nog niet is bereikt het volgende
+        teller += 1
+        if (n & (n-1)) == 0:    # als factor2
+            n /= 2
+        else:                   # Geen factor van 2
+            for i in range(n - 1, 2, -1):       # Zoek eerstvolgende factor 2 getal, terugtellend.
+                if (i & (i - 1)) == 0:  # Factor 2 getal gevonden
+                    n -= i                          # Van n nu de kleinere factor2 aftrekken
+    if teller % 2 == 0:
+        return "Richard"
+    else:
+        return "Louise"
 
 
 
