ca9af591dcb8d4ca95bdcb48f87b1b66d3afc85d
dstada <dickstada@gmail.com>
2019-04-04 14:15:28 +0200

    ready

---
+++ Smart Number - Debugging - Algorythms.py
@@ -0,0 +1,64 @@
+
+import math
+
+def is_smart_number(num):
+    teller = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            teller += 1
+    if teller % 2 == 0:
+        return False
+    else:
+        return True
+
+
+
+
+for _ in range(int(input())):
+    num = int(input())
+    ans = is_smart_number(num)
+    if ans:
+        print("YES")
+    else:
+        print("NO")
+
+
+"""
+https://www.hackerrank.com/challenges/smart-number/problem
+
+input:
+4
+1
+2
+7
+169
+
+output:
+YES
+NO
+NO
+YES
+
+
+    val = int(math.sqrt(num))
+    if num / val == 1:
+        return True
+    return False
+    
+    
+    
+def is_smart_number(num):
+    teller = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            teller += 1
+    if teller % 2 == 0:
+        return "NO"
+    else:
+        return "YES"
+
+
+for _ in range(int(input())):
+    num = int(input())
+    print(is_smart_number(num))
+"""
