284be9f92cd9bef16ac21676a3970201e22b48ed
Dick Stada <dickstada@gmail.com>
2019-05-28 14:19:35 +0200

    Add files via upload

--- 3D Surface Area - Algorythms - Hackerrank.py
+++ 3D Surface Area - Algorythms - Hackerrank.py
@@ -10,7 +10,32 @@ Output
 """
 
 def surfaceArea(A):
-    print("hoi")
+    print(A)
+    surfaces = 0
+    for i in range(len(A)): # Voor elke regel invoer:
+        if i == 0 or i == len(A)-1:
+            print("voor/achterkant: {}".format(sum(A[i])))
+            surfaces += sum(A[i])
+        for j in range(len(A[i])):  # Voor elke kolom in de regel:
+            print(A[i][j])
+            if j == 0 or j == len(A[i])-1: # De vlakken aan rechter- en linkerkant
+                surfaces += A[i][j]
+            surfaces += 2   # Voor boven- en onderkant
+            if j > 0:   # alleen 2e t/m rest
+                # Absolute verschil met vorige:
+                surfaces = surfaces + abs(A[i][j] - A[i][j-1])
+        print(surfaces)
+    # Areas between front and back:
+    # for k in range(len(A)):
+    print("------------------------")
+
+    for l in range(len(A[0])):
+        print("l: {}".format(l))
+        for m in range(len(A)):
+            print("m: {}".format(m))
+
+
+
 
 if __name__ == '__main__':
     HW = input().split()
