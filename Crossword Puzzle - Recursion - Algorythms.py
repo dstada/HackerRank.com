6a85d8ab8fa440915f11db47a98f3314c0816e87
Dick Stada <dickstada@gmail.com>
2019-06-13 14:18:16 +0200

    Add files via upload

--- Crossword Puzzle - Recursion - Algorythms.py
+++ Crossword Puzzle - Recursion - Algorythms.py
@@ -1,32 +1,36 @@
-
-import os
+"""
+https://www.hackerrank.com/challenges/crossword-puzzle/problem
+"""
 
 
 def crosswordPuzzle(crossword, words):
+    vakken = []
     print(crossword)
     print("-------------")
     print(words)
     for i in range(len(crossword)):
         print(crossword[i])
-        for j in range(0,11):
-
+        temp = []
+        for j in range(0,10):
+            print(crossword[i][j])
+            if crossword[i][j] == '-':
+                temp.append([i,j])
+        if temp != [] and len(temp) > 1:
+            vakken.append(temp)
+    print(vakken)
 
 
 if __name__ == '__main__':
     crossword = []
-
     for _ in range(10):
         crossword_item = input()
         crossword.append(crossword_item)
-
     words = input()
-
     result = crosswordPuzzle(crossword, words)
     print(result)
 
 """
 Input 	   		
-
 ++++++++++
 +------+++
 +++-++++++
@@ -40,7 +44,6 @@ Input
 POLAND;LHASA;SPAIN;INDIA
 
 Output:
-
 ++++++++++
 +POLAND+++
 +++H++++++
@@ -51,5 +54,80 @@ Output:
 ++++++I+++
 ++++++A+++
 ++++++++++
+
+Input:
++-++++++++
++-++++++++
++-++++++++
++-----++++
++-+++-++++
++-+++-++++
++++++-++++
+++------++
++++++-++++
++++++-++++
+LONDON;DELHI;ICELAND;ANKARA
+
+Output:
++L++++++++
++O++++++++
++N++++++++
++DELHI++++
++O+++C++++
++N+++E++++
++++++L++++
+++ANKARA++
++++++N++++
++++++D++++
+
+Input:
++-++++++++
++-++++++++
++-------++
++-++++++++
++-++++++++
++------+++
++-+++-++++
++++++-++++
++++++-++++
+++++++++++
+AGRA;NORWAY;ENGLAND;GWALIOR
+
+Output:
++E++++++++
++N++++++++
++GWALIOR++
++L++++++++
++A++++++++
++NORWAY+++
++D+++G++++
++++++R++++
++++++A++++
+++++++++++
+
+Input:
+XXXXXX-XXX
+XX------XX
+XXXXXX-XXX
+XXXXXX-XXX
+XXX------X
+XXXXXX-X-X
+XXXXXX-X-X
+XXXXXXXX-X
+XXXXXXXX-X
+XXXXXXXX-X
+ICELAND;MEXICO;PANAMA;ALMATY
+
+Output:
+XXXXXXIXXX
+XXMEXICOXX
+XXXXXXEXXX
+XXXXXXLXXX
+XXXPANAMAX
+XXXXXXNXLX
+XXXXXXDXMX
+XXXXXXXXAX
+XXXXXXXXTX
+XXXXXXXXYX
 
 """
