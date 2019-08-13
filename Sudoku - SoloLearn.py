8e8974f7e623c0bc703a6c17d2a9f10dc663b8e4
Dick Stada <dickstada@gmail.com>
2019-08-13 21:33:36 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

--- Sudoku - SoloLearn.py
+++ Sudoku - SoloLearn.py
@@ -1,25 +1,14 @@
-"""
+"""Sudoku Validator (SoloLearn)
+
 Create an algorithm that checks any solved sudoku's validity.
-Sudoku Validator
+
 A solved sudoku puzzle is a matrix of typically 9*9 in which every row,
 every column and every block contains every number from 1 to 9 exactly once.
 
 Dick STADA - May 2019
 """
 
-# solution = [            # output: True
-#     [3, 5, 2, 9, 1, 8, 6, 7, 4],
-#     [8, 9, 7, 2, 4, 6, 5, 1, 3],
-#     [6, 4, 1, 7, 5, 3, 2, 8, 9],
-#     [7, 8, 3, 5, 6, 9, 4, 2, 1],
-#     [9, 2, 6, 1, 3, 4, 7, 5, 8],
-#     [4, 1, 5, 8, 2, 7, 9, 3, 6],
-#     [1, 6, 4, 3, 7, 5, 8, 9, 2],
-#     [2, 7, 8, 4, 9, 1, 3, 6, 5],
-#     [5, 3, 9, 6, 8, 2, 1, 4, 7]
-#   ]
-
-solution = [            # output: False (vertically not OK (6th column))
+solution = [            # output: True
     [3, 5, 2, 9, 1, 8, 6, 7, 4],
     [8, 9, 7, 2, 4, 6, 5, 1, 3],
     [6, 4, 1, 7, 5, 3, 2, 8, 9],
@@ -28,8 +17,20 @@ solution = [            # output: False (vertically not OK (6th column))
     [4, 1, 5, 8, 2, 7, 9, 3, 6],
     [1, 6, 4, 3, 7, 5, 8, 9, 2],
     [2, 7, 8, 4, 9, 1, 3, 6, 5],
-    [2, 7, 8, 4, 9, 1, 3, 6, 5],
+    [5, 3, 9, 6, 8, 2, 1, 4, 7]
   ]
+
+# solution = [            # output: False (vertically not OK (6th column))
+#     [3, 5, 2, 9, 1, 8, 6, 7, 4],
+#     [8, 9, 7, 2, 4, 6, 5, 1, 3],
+#     [6, 4, 1, 7, 5, 3, 2, 8, 9],
+#     [7, 8, 3, 5, 6, 9, 4, 2, 1],
+#     [9, 2, 6, 1, 3, 4, 7, 5, 8],
+#     [4, 1, 5, 8, 2, 7, 9, 3, 6],
+#     [1, 6, 4, 3, 7, 5, 8, 9, 2],
+#     [2, 7, 8, 4, 9, 1, 3, 6, 5],
+#     [2, 7, 8, 4, 9, 1, 3, 6, 5],
+#   ]
 
 # solution = [            # output: False  (Horizontal not OK (7th line))
 #     [3, 5, 2, 9, 1, 8, 6, 7, 4],
