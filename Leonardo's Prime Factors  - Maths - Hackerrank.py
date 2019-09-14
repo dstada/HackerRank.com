c56c6868e904fc38c4c7c24edf12e66fb53b1971
Dick Stada <dickstada@gmail.com>
2019-09-14 09:39:56 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

--- Leonardo's Prime Factors  - Maths - Hackerrank.py
+++ Leonardo's Prime Factors  - Maths - Hackerrank.py
@@ -3,37 +3,15 @@ For each n , he wants you to count the maximum number of unique prime factors of
 range [1,n] and then print this value on a new line.
 """
 
-
-def check_prime(n):      # checks is n is a prime
-    if (n <= 1):
-        return False
-    if (n <= 3):
-        return True
-    if (n % 2 == 0 or n % 3 == 0):
-        return False
-    i = 5
-    while (i * i <= n):
-        if (n % i == 0 or n % (i + 2) == 0):
-            return False
-        i = i + 6
-    return True
-
-
 def primeCount(n):
-    number_of_primes = 0
-    total = 0
-    for i in range(3, n//2):
-        if check_prime(i):
-            print(i)
-            if total == 0:
-                total = i
-            else:
-                total *= i
-            print("total: {}".format(total))
-            number_of_primes += 1
-            print("nr of primes: {}".format(number_of_primes))
-            if total > n:
-                return number_of_primes - 1
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    for i in range(n):
+        count,result = 0, 1
+        for j in primes:
+            result *= j
+            if result <= n:
+                count += 1
+        return count
 
 
 
