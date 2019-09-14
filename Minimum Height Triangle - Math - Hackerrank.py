699584fae71b60ade1d32cbcb5cc9dabf6422458
Dick Stada <dickstada@gmail.com>
2019-09-14 19:34:42 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

---
+++ Minimum Height Triangle - Math - Hackerrank.py
@@ -0,0 +1,13 @@
+import math
+
+
+def lowestTriangle(base, area):
+    return math.ceil(2 * area / base)
+
+
+base, area = input().strip().split(' ')
+base, area = [int(base), int(area)]
+height = lowestTriangle(base, area)
+print(height)
+
+# https://www.hackerrank.com/challenges/lowest-triangle/problem
