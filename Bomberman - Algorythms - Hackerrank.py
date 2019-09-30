60064f710a068fa010a84fe1060ff0405afa6903
Dick Stada <dickstada@gmail.com>
2019-09-30 21:39:12 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

--- Bomberman - Algorythms - Hackerrank.py
+++ Bomberman - Algorythms - Hackerrank.py
@@ -1,3 +1,27 @@
+def detonation(grid):
+    grid3 = []
+    for i in range(len(grid)):
+        # print("Rij {}".format(i))
+        # print(grid[i])
+        rule = ""
+        for j in range(len(grid[0])):
+            if grid[i][j] == "O":  # Here is a bomb
+                rule += "."
+            elif j > 0 and grid[i][j - 1] == "O":  # Left of it is a bomb
+                rule += "."
+            elif j < len(grid[0]) - 1 and grid[i][j + 1] == "O":  # Right is a bomb
+                rule += "."
+            elif i > 0 and grid[i - 1][j] == "O":  # Above is a bomb
+                rule += "."
+            elif i < len(grid) - 1 and grid[i + 1][j] == "O":  # Beneath it is a bomb
+                rule += "."
+            else:
+                rule += "O"
+        print(rule)
+        grid3.append(rule)
+    return grid3
+
+
 def bomberMan(n, grid):
     print(grid)
 
@@ -9,30 +33,13 @@ def bomberMan(n, grid):
         return grid     # Return the same grid as the input grid
 
     if n % 4 == 3:      # s = 3, 7, 11, 15, etc. After detonation of the bombs.
-        grid3 = []
-        for i in range(len(grid)):
-            print("Rij {}".format(i))
-            print(grid[i])
-            rule = ""
-            for j in range(len(grid[0])):
-                if grid[i][j] == "O":                   # Here is a bomb
-                    rule += "."
-                elif j > 0 and grid[i][j-1] == "O":     # Left of it is a bomb
-                    rule += "."
-                elif j < len(grid[0]) - 1 and grid[i][j+1] == "O":   # Right is a bomb
-                    rule += "."
-                elif i > 0 and grid[i-1][j] == "O":     # Above is a bomb
-                    rule += "."
-                elif i < len(grid) - 1 and grid[i + 1][j] == "O":   # Beneath it is a bomb
-                    rule += "."
-                else:
-                    rule += "O"
-            print(rule)
-            grid3.append(rule)
-        print(grid3)
-        return grid3
+        grid4 = detonation(detonation(grid))
+        return grid4
 
-    # return set3     # s = 5, 9, 13, 17, enz
+    print("Kennelijk is n nu 5, 9, 13, enz")
+    grid4 = detonation(grid)     # s = 5, 9, 13, 17, enz. So, the situation after detonation in grid3.
+    grid5 = detonation(grid4)
+    return grid5
 
 
 if __name__ == '__main__':
