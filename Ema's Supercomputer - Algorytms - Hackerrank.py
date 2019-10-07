327a0c632cdbc52292db9a1b8728b09b09b2648a
Dick Stada <dickstada@gmail.com>
2019-10-07 21:46:22 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

--- Ema's Supercomputer - Algorytms - Hackerrank.py
+++ Ema's Supercomputer - Algorytms - Hackerrank.py
@@ -2,24 +2,42 @@
 
 def twoPluses(grid):
     print(grid)
-    plusses = []
-    for i in range(1, len(grid)-1):
+    max_area = 1
+    for i in range(1, len(grid)-1):         # Elk cel langs die niet aan de rand zit
         for j in range(1, len(grid[0])-1):
+            # Bepaal voor deze cel de grootte van de plus
             print(str(grid[i][j]))
-            print(i, ",", j)
+            print(i, "-", j)
             # Bepaal hoe groot de plus maximaal kan zijn:
             min_abov_undr = min((len(grid)-int(i)-1), i)
             min_left_right = min(j, (len(grid[0])-int(j))-1)
-            maximum = min(min_abov_undr, min_left_right)
-            print("Plus kan maximaal {} cel(len) rond centrale cel groot zijn".format(maximum))
-            for max in range(1, maximum+1):
-                print(max)
+            maximum = min(min_abov_undr, min_left_right)        # Plus kan maximaal [maximum] groot zijn.
+            plusgrootte = 0
+            area = 0
+            for max in range(1, maximum+1):     # Hoe groot is de maximale plus
                 if grid[i-max][j] == "G" and grid[i+max][j] == "G" and grid[i][j-max] == "G" and grid[i][j+max] == "G":
                     print("Extra cel!")
+                    plusgrootte += 1
+                    area = (4 * plusgrootte) + 1
                 else:
                     break
+            # print("plusgrootte: {}".format(plusgrootte))
+            print("area voor deze cel: {}".format(area))
+            if area > max_area:
+                max_area = area
+            # Nu een nieuw grid maken met huidige plus als "D" in de cellen:
+
+
+            # Nu opnieuw de grootste area berekenen
+            # eerste area * nieuwe grootste area onthouden.
+            # Als dat bij een volgende cel groter is, wordt dat het nieuwe totaal.
+
+
             print("-----------------------------")
 
+
+
+    print("max_area: {}".format(max_area))
 
 if __name__ == '__main__':
     nm = input().split()
