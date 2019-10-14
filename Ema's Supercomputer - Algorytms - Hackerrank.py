a0b242123b85e567f5aeb7d17117c7b42e77bdbc
Dick Stada <dickstada@gmail.com>
2019-10-14 22:03:04 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

--- Ema's Supercomputer - Algorytms - Hackerrank.py
+++ Ema's Supercomputer - Algorytms - Hackerrank.py
@@ -69,55 +69,8 @@ def twoPluses(grid):
     print("max_area: {}".format(max_area))
 
 # TODO: Als er alleen G's in de randcellen zitten: check of er 2 G's in zitten. Return is dan 1
-"""
-def twoPluses(grid):
-    # max_area = 1
-    grid_new = grid
-    for i in range(1, len(grid)-1):             # Elk regel langs
-        for j in range(1, len(grid[0])-1):      # Elke letter van de regel
-            if grid[i][j] == "G":
-                print("Grid: {}".format(grid))
-                # grid_new = ['GGGGGG', 'GGGGGG', 'GGGGGG', 'GGGGGG', 'GGGGGG']
-                # grid_new[i] = grid_new[i][:j] + "B" + grid_new[i][j + 1:]
-                grid_new[i] = grid[i][:j] + "B" + grid[i][j + 1:]
-                print("gr_n: {}".format(grid_new))
-                # Bepaal voor deze cel de grootte van de plus
-                # print(str(grid[i][j]))
-                # print(i, "-", j)
-                # Bepaal hoe groot de plus maximaal kan zijn:
-                # min_abov_undr = min((len(grid)-int(i)-1), i)
-                # min_left_right = min(j, (len(grid[0])-int(j))-1)
-                # maximum = min(min_abov_undr, min_left_right)        # Plus kan maximaal [maximum] groot zijn.
-                # plusgrootte = 0
-                # area = 0
-                # grid_new[i] = grid_new[i][:j] + "B" + grid_new[i][j + 1:]  # de cel zelf (is dat wel nodig?)
-                # print(grid_new[i])
-                # for max in range(1, maximum+1):     # Hoe groot is de maximale plus
-                #     if grid[i-max][j] == "G" and grid[i+max][j] == "G" and grid[i][j-max] == "G" and grid[i][j+max] == "G":
-                #         print("Extra cel!")
-                #         # De G's veranderen in B's:
-                #         grid_new[i-max] = grid_new[i-max][:j] + "B" + grid_new[i-max][j+1:]    # cel boven bewuste cel
-                #         grid_new[i+max] = grid_new[i+max][:j] + "B" + grid_new[i-max][j+1:]    # cel onder bewuste cel
-                #         grid_new[i] = grid_new[i][:j - max] + "B" + grid_new[i][j - max + 1:]  # cel links
-                #         grid_new[i] = grid_new[i][:j + max] + "B" + grid_new[i][j + max + 1:]  # cel rechts
-                #         plusgrootte += 1
-                #         area = (4 * plusgrootte) + 1
-                #     else:
-                #         break
-                # print("Bij deze plus is de area: {}".format(area))
-                # print(grid_new)
-                # if area > max_area:
-                #     max_area = area
-                # Nu een nieuw grid maken met huidige plus als "D" in de cellen:
-                # Neem cel voor cel over uit oorspronkelijke grid. Als cel in plus: maak een "B"
 
 
-                # Nu opnieuw de grootste area berekenen
-                # eerste area * nieuwe grootste area onthouden.
-                # Als dat bij een volgende cel groter is, wordt dat het nieuwe totaal.
-                print("-----------------------------")
-    # print("max_area: {}".format(max_area))
-"""
 
 if __name__ == '__main__':
     nm = input().split()
@@ -182,5 +135,9 @@ BGGGGG
 BBBGBB
 BBBGBB
 BBBBBB
+
+2 2
+BG
+BB
 
 """
