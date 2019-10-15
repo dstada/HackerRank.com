2af079157710c0b090215027539cfa402021de1b
Dick Stada <dickstada@gmail.com>
2019-10-15 22:48:28 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

--- matrix layer rotation  - Algorythms - Hackerrank.py
+++ matrix layer rotation  - Algorythms - Hackerrank.py
@@ -2,8 +2,26 @@
 
 def matrixRotation(matrix, r):
     print(matrix)
-    print("Aantal schillen: {}".format(int(min(len(matrix), len(matrix[0]))/2)))
-
+    schil1 = []
+    regels = len(matrix)
+    kolommen = len(matrix[0])
+    print("Aantal schillen: {}".format(int(min(regels, kolommen)/2)))
+    # Maak list van buitenste schil:
+    # Eerste regel toevoegen:
+    for l in range(kolommen):
+        schil1.append(matrix[0][l])
+    # laatste elementen van tussenregels toevoegen:
+    for i in range(1, regels - 1):
+        laatste = matrix[i][-1]
+        schil1.append(laatste)
+    # laatste regel omgekeerd toevoegen:
+    # for m in range(regels)
+    schil1.append(matrix[regels-1][::-1])
+    # eerste elementen van tussenregels toevoegen:
+    for j in range(regels - 2, 0, -1):
+        eerste = matrix[j][:1]
+        schil1.append(eerste)
+    print(schil1)
 
 
 if __name__ == '__main__':
