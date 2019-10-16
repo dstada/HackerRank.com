70f276c1164519d1d37ac0d7607e83d5df468fdc
Dick Stada <dickstada@gmail.com>
2019-10-16 21:17:33 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Icecream Parlor - Logarythms.py

--- matrix layer rotation  - Algorythms - Hackerrank.py
+++ matrix layer rotation  - Algorythms - Hackerrank.py
@@ -2,12 +2,12 @@
 
 def matrixRotation(matrix, r):
     print(matrix)
-    schil1 = []
     regels = len(matrix)
     kolommen = len(matrix[0])
     print("Aantal schillen: {}".format(int(min(regels, kolommen)/2)))
-    # Maak list van buitenste schil:
+    # Buitenste schil:
     # Eerste regel toevoegen:
+    schil1 = []
     for l in range(kolommen):
         schil1.append(matrix[0][l])
     # laatste elementen van tussenregels toevoegen:
@@ -15,13 +15,35 @@ def matrixRotation(matrix, r):
         laatste = matrix[i][-1]
         schil1.append(laatste)
     # laatste regel omgekeerd toevoegen:
-    # for m in range(regels)
-    schil1.append(matrix[regels-1][::-1])
+    for q in matrix[regels-1][::-1]:
+        schil1.append(q)
     # eerste elementen van tussenregels toevoegen:
     for j in range(regels - 2, 0, -1):
-        eerste = matrix[j][:1]
+        eerste = matrix[j][0]
         schil1.append(eerste)
+    # roteren van de schil met een plek:
+    schil1.append(schil1.pop(0))
     print(schil1)
+
+    # Binnenste (tweede) schil:
+    schil2 = []
+    # Eerste regel toevoegen:
+    for a in range(1, kolommen - 1):
+        schil2.append(matrix[1][a])
+    # Rechterkolom toevoegen:
+    for p in range(2, regels - 2):
+        laatst = matrix[p][kolommen - 2]
+        schil2.append(laatst)
+    # Onderste regel omgekeerd toevoegen:
+    for b in range(kolommen - 2, 0, -1):
+        schil2.append(matrix[regels - 2][b])
+    # eerste elementen van tussenregels toevoegen:
+    for v in range(regels - 3, 1, -1):
+        eerst = matrix[v][1]
+        schil2.append(eerst)
+    # roteren van de schil met een plek:
+    schil2.append(schil2.pop(0))
+    print(schil2)
 
 
 if __name__ == '__main__':
@@ -40,7 +62,6 @@ if __name__ == '__main__':
 
 """https://www.hackerrank.com/challenges/matrix-rotation-algo/problem
 Input:
-
 4 4 2
 1 2 3 4
 5 6 7 8
@@ -48,9 +69,31 @@ Input:
 13 14 15 16
 
 Output:
-
 3 4 8 12
 2 11 10 16
 1 7 6 15
 5 9 13 14
+
+Input:
+5 4 7
+1 2 3 4
+7 8 9 10
+13 14 15 16
+19 20 21 22
+25 26 27 28
+
+Output:
+28 27 26 25
+22 9 15 19
+16 8 21 13
+10 14 20 7
+4 3 2 1
+
+5 5 1
+1 2 3 4 5
+6 7 8 9 10
+11 12 13 14 15
+16 17 18 19 20
+21 22 23 24 25
+
 """
