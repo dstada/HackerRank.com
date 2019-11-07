9482a4cad672f4e7da8ad1b1e86f9d7530087146
Dick Stada <dickstada@gmail.com>
2019-11-07 22:30:58 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    # Icecream Parlor - Logarythms.py

--- teksten.py
+++ teksten.py
@@ -1,2 +1,28 @@
-inputFileText = open("C:\Users\Dick Stada\Downloads\OpenTaal-210G-woordenlijsten\OpenTaal-210G-basis-gekeurd.txt", "r").read()
-print (inputFileText)
+inputFileText = open("C:/Users/Dick Stada/Downloads/OpenTaal-210G-woordenlijsten/OpenTaal-210G-basis-gekeurd.txt", "r").read()
+# print (inputFileText)   # de hele inhoud printen
+if "ontnieter" in inputFileText:
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+for line in open("C:/Users/Dick Stada/Downloads/OpenTaal-210G-woordenlijsten/OpenTaal-210G-basis-gekeurd.txt", "r").readlines():
+# for line in open("C:/Users/Dick Stada/Downloads/OpenTaal-210G-woordenlijsten/OpenTaal-210G-basis-ongekeurd.txt", "r").readlines():
+# for line in open("C:/Users/Dick Stada/Downloads/OpenTaal-210G-woordenlijsten/OpenTaal-210G-verwarrend.txt", "r").readlines():
+# for line in open("C:/Users/Dick Stada/Downloads/OpenTaal-210G-woordenlijsten/OpenTaal-210G-flexievormen.txt", "r").readlines():
+#                      "r").readlines():
+#     print(line.strip())
+#     print(len(line))
+    line = line.strip()
+
+    if len(line) == 9:
+        # print(line)
+        line_new = line[0] + line[3] + line[6] + line[1] + line[4] + line[7] + line[2] + line[5] + line[8]
+        if line.strip() == line_new:
+            print(line)
+        if line_new in inputFileText:
+            print(line, line_new)
+
+    # if len(line) == 16:
+    #     # print(line)
+    #     line_new = line[0] + line[4] + line[8] + line[12] + line[1] + line[5] + line[9] + line[13] + line[2] + line[6] + line[10] + line[14] + line[2] + line[3] + line[7] + line[11]
+    #     if line.strip() == line_new:
+    #         print(line)
+
